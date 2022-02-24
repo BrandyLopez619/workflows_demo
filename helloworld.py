a0ae51a9252bbd940d56fb31df2e1b0480495859
Brandy Lopez <brandylopez@myobra.com>
2022-02-23 18:22:37 -0800

    add helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,17 @@
+"""
+echo "# workflows_demo" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/BrandyLopez619/workflows_demo.git
+git push -u origin main
+"""
+
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
