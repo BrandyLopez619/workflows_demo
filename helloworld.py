b18ba897da8de8e1d069b8b6f56d28aba77ab806
Brandy Lopez <96094840+BrandyLopez619@users.noreply.github.com>
2022-02-23 18:38:02 -0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,13 @@
+import sys
+
+def main():
+    print("Hello World! From Python: " + str(sys.version_info))
+    if sys.version_info >= (3, 6) and sys.version_info < (3, 7):
+        # let's make this script fail for Python 3.6
+        raise Exception('Python version 3.6.x is unsupported!')
+
+if __name__ == "__main__":
+    main()
 """
 echo "# workflows_demo" >> README.md
 git init
@@ -6,7 +16,7 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/BrandyLopez619/workflows_demo.git
 git push -u origin main
-"""
+
 
 
 def main():
@@ -15,3 +25,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+"""
